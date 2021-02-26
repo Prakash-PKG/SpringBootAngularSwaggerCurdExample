4245be98192fb50d8f543dc841d82bcf2c080ca7
Prakash-PKG <prakash.garanaik18@gmail.com>
2021-02-27 00:15:50 +0530

    Code updated

--- src/main/java/com/app/pk/SpringBootAngularSwaggerCurdExampleApplication.java
+++ src/main/java/com/app/pk/SpringBootAngularSwaggerCurdExampleApplication.java
@@ -9,6 +9,7 @@ public class SpringBootAngularSwaggerCurdExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularSwaggerCurdExampleApplication.class, args);
           int a=10;
+           int b=20;
 	}
 
 }
